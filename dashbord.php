0d8a53ab47cb5d2d0b69665bc442a03717c5e1c4
MadelineHenry <madelinehenry04101996@gmail.com>
2021-01-12 11:09:25 +0100

    generate the cards in javascript

--- dashbord.php
+++ dashbord.php
@@ -40,57 +40,23 @@
         <div class="row">
             <div class="col-3" id="profilPart">
                 <h1>My profil</h1>
-                <img src="" alt="">
+                <img src="./assets/imgs/unnamed.png" alt="photoUtilisateur">
                 <p>NOM PRENOM</p>
                 <p>EMAIL@GMAIL.COM</p>
                 </p>
             </div>
-            <div class="col-8" id="recipeList">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Title of recipe</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">By nom prenom</h6>
-                        <h7 class="card-subtitle mb-2 text-muted">Date</h7>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <div class='col-lg-12 grandBlocnote'>
-                            <div class='note5' data-count='5'>★</div>
-                            <div class='note4' data-count='4'>★</div>
-                            <div class='note3' data-count='3'>★</div>
-                            <div class='note2' data-count='2'>★</div>
-                            <div class='note1' data-count='1'>★</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Title of recipe</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">By nom prenom</h6>
-                        <h7 class="card-subtitle mb-2 text-muted">Date</h7>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <div class='col-lg-12 grandBlocnote'>
-                            <div class='note5' data-count='5'>★</div>
-                            <div class='note4' data-count='4'>★</div>
-                            <div class='note3' data-count='3'>★</div>
-                            <div class='note2' data-count='2'>★</div>
-                            <div class='note1' data-count='1'>★</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <!-- List recipe -->
+            <div class="col-9" id="recipeList"></div>
         </div>
 
     </div>
 
 
-    <!-- List recipe -->
-
     <!-- javascript bootstrap -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    <script src="./script.js"></script>
 </body>
 
 </html>
---
+++ script.js
@@ -0,0 +1,49 @@
+function createCard(){
+        let recipeList = document.getElementById("recipeList")
+
+    let divCard = document.createElement("div")
+    divCard.className = "card"
+    recipeList.appendChild(divCard)
+
+    let cardBody= document.createElement("div")
+    cardBody.className = "card-body"
+    divCard.appendChild(cardBody)
+
+    let cardTitle = document.createElement("h5")
+    cardTitle.className = "card-title"
+    cardTitle.textContent = "Title of recipe"
+    cardBody.appendChild(cardTitle)
+
+
+    let byWho = document.createElement("h6")
+    byWho.className = "card-subtitle mb-2 text-muted"
+    byWho.textContent = "LastName FirstName"
+    cardBody.appendChild(byWho)
+
+    let addDate = document.createElement("h7")
+    addDate.className = "card-subtitle mb-2 text-muted"
+    addDate.textContent = "Date"
+    cardBody.appendChild(addDate)
+
+    let cardText= document.createElement("p")
+    cardText.className = "card-text"
+    cardText.textContent = "Some quick example text to build on the card title and make up the bulk of card s content."
+    cardBody.appendChild(cardText)
+
+    let stars = document.createElement("div")
+    stars.className = "col-lg-12 grandBlocnote"
+    cardBody.appendChild(stars)
+
+    //créer les étoiles jusque 5
+    for (let i=1; i<6; i++){
+
+        let star = document.createElement("div")
+        star.className = "note"+ i
+        star.dataCount = i
+        star.textContent = "★"
+        stars.appendChild(star)
+
+    }
+}
+
+createCard();
