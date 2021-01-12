12e9a65fe969ec9af812d440c2c98c4075ab16f7
Agnes Kabongo <57146479+agkabongo@users.noreply.github.com>
2021-01-12 17:15:40 +0100

    rating stars

--- modal.php
+++ modal.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <script src="https://use.fontawesome.com/b3178bb50e.js"></script>
     <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <title>beacuz</title>
 </head>
@@ -53,6 +53,24 @@
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="difficulty">Difficulty</label>
+                                <div class="container d-flex justify-content-center mt-200">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="stars">
+                                                <form action=""> <input class="star star-5" id="star-5" type="radio"
+                                                        name="star" /> <label class="star star-5" for="star-5"></label>
+                                                    <input class="star star-4" id="star-4" type="radio" name="star" />
+                                                    <label class="star star-4" for="star-4"></label> <input
+                                                        class="star star-3" id="star-3" type="radio" name="star" />
+                                                    <label class="star star-3" for="star-3"></label> <input
+                                                        class="star star-2" id="star-2" type="radio" name="star" />
+                                                    <label class="star star-2" for="star-2"></label> <input
+                                                        class="star star-1" id="star-1" type="radio" name="star" />
+                                                    <label class="star star-1" for="star-1"></label> </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
@@ -63,20 +81,20 @@
                             </div>
                         </div>
                         <div class="form-row">
-                        <div class=" form-group modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
+                            <div class=" form-group modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Save changes</button>
+                            </div>
                         </div>
+                    </form>
                 </div>
-                </form>
             </div>
         </div>
     </div>
-    </div>
 
     <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/rating.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script href="./script.js"></script>
 </body>
 
 </html>
